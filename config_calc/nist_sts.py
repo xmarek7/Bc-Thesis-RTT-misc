67eb83d3ce7538cf13f9e782440b825166eda51b
Tomáš MAREK <514257@mail.muni.cz>
2023-04-14 11:06:03 +0200

    initial commit

---
+++ config_calc/nist_sts.py
@@ -0,0 +1,11 @@
+def nist_sts_test(file_size: int):
+    # no test set for execution
+    if file_size < 131072:
+        return {}
+    return {
+        "defaults":{
+            "test-ids": ["1-15"],
+            "stream-size": "1000000",
+            "stream-count": str(file_size // 131072)
+        }
+    }
