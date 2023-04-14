ed548e3a04bc194b74dcf9472e3d1332098e17c6
Tomáš MAREK <514257@mail.muni.cz>
2023-04-14 20:35:51 +0200

    added test-specific-settings

--- config_calc/nist_sts.py
+++ config_calc/nist_sts.py
@@ -7,5 +7,6 @@ def nist_sts_test(file_size: int):
             "test-ids": ["1-15"],
             "stream-size": "1000000",
             "stream-count": str(file_size // 131072)
-        }
+        },
+        "test-specific-settings" : []
     }
