b36029ebd0cec73980ba9607f319f8c094fa62b2
Tomáš MAREK <514257@mail.muni.cz>
2023-05-17 14:46:47 +0200

    fixed so stream size can be changed

--- config_calc/nist_sts.py
+++ config_calc/nist_sts.py
@@ -1,12 +1,12 @@
-def nist_sts_test(file_size: int):
+def nist_sts_test(args, file_size: int):
     # no test set for execution
-    if file_size < 131072:
-        return {}
+    if file_size < args.nist_stream_size:
+        return {"comment": "Given NIST stream size is too big."}
     return {
         "defaults":{
             "test-ids": ["1-15"],
-            "stream-size": "1000000",
-            "stream-count": str(file_size // 131072)
+            "stream-size": str(args.nist_stream_size),
+            "stream-count": str((file_size * 8) // args.nist_stream_size)
         },
         "test-specific-settings" : []
     }
