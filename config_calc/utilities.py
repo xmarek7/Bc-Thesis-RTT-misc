e5d81fd680a4a67a1af7e9596851c0a735e8cc36
Tomáš MAREK <514257@mail.muni.cz>
2023-10-27 10:48:16 +0200

    added parsing for size in format integer+unit

--- config_calc/utilities.py
+++ config_calc/utilities.py
@@ -1,5 +1,29 @@
-from typing import List
+from typing import List, Optional
+from re import compile, fullmatch
 
+KILO = 1024
+MEGA = KILO ** 2
+GIGA = KILO ** 3
+TERA = KILO ** 4
+
+def parse_size(input: str) -> Optional[int]:
+    if input.isnumeric():
+        return int(input)
+    regex = compile("^[0-9]+[kKmMgGtT]$")
+    if regex.fullmatch(input) is None:
+        return None
+
+    unit = input[-1]
+    if unit in "kK":
+        power = KILO
+    elif unit in "mM":
+        power = MEGA
+    elif unit in "gG":
+        power = GIGA
+    else:
+        power = TERA
+    return int(input[:-1]) * power
+    
 
 def concacenate_test_ids(test_ids: List[int]) -> List[str]:
     concat = []
